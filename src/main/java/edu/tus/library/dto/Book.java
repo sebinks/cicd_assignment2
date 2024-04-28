b7d61daadffaea99b7b3949075217af2cad485c9
sebinks <33017777+sebinks@users.noreply.github.com>
2024-04-28 13:18:29 +0100

    Update Book.java

--- src/main/java/edu/tus/library/dto/Book.java
+++ src/main/java/edu/tus/library/dto/Book.java
@@ -1,8 +1,14 @@
 package edu.tus.library.dto;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+// import javax.persistence.Column;
+// import javax.persistence.Entity;
+// import javax.persistence.Id;
+// import javax.persistence.Table;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 
 @Entity
 public class Book {
